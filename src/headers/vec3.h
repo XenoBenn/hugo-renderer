97c0bd5531f466a50a9f60d35e2e1fc4c5f8abbe
XenoBenn <benjamin.komar@gmail.com>
2023-03-26 16:27:13 +0200

    different diffuse formulas

--- src/headers/vec3.h
+++ src/headers/vec3.h
@@ -54,18 +54,12 @@ class vec3 {
             return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
         }
 
+
     public:
         double e[3];
 };
 
-// Rejection method
-vec3 random_in_unit_sphere() {
-    while (true) {
-        auto p = vec3::random(-1,1);
-        if (p.length_squared() >= 1) continue;
-        return p;
-    }
-}
+
 
 // Type aliases for vec3
 using point3 = vec3;   // 3D point
@@ -117,6 +111,26 @@ inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
 
+// Rejection method
+    vec3 random_in_unit_sphere() {
+        while (true) {
+        auto p = vec3::random(-1,1);
+            if (p.length_squared() >= 1) continue;
+                return p;
+        }
+    }
+
+    vec3 random_unit_vector() {
+        return unit_vector(random_in_unit_sphere());
+    }
+
+    vec3 random_in_hemisphere(const vec3& normal) {
+    vec3 in_unit_sphere = random_in_unit_sphere();
+        if (dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+            return in_unit_sphere;
+        else
+            return -in_unit_sphere;
+}
 
 
 #endif
